4f3703dac8c9a4f2136990d736718bc7330001fc
evan <63502524+evanmamesah@users.noreply.github.com>
2023-11-13 21:48:21 +0800

    Update my photo

--- src/components/section1/index.jsx
+++ src/components/section1/index.jsx
@@ -5,7 +5,7 @@ const Section1 = () => {
           <div className="avatar">
             <img
               className="img-fluid rounded-circle"
-              src="https://github.com/evanmamesah/me/blob/main/src/assets/img/avatar.jpg?raw=true"
+              src="https://github.com/evanmamesah/me/blob/main/src/assets/img/avatar1.jpg?raw=true"
             />
           </div>
           <p>
