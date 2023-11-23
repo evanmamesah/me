d069c9194a85274bd8df3baf3ddf8829296cac32
evan <63502524+evanmamesah@users.noreply.github.com>
2023-11-23 11:13:08 +0800

    Update information in section1 (for the many times).jsx

--- src/components/section1/index.jsx
+++ src/components/section1/index.jsx
@@ -9,7 +9,7 @@ const Section1 = () => {
             />
           </div>
           <p>
-            Hello! I am <strong>Evan Mamesah</strong>. I am a student in Klabat University.
+            Hello! I am <strong>Evan Mamesah</strong>. I am a Computer Science student majoring Information System in Klabat University.
             I have passion for coding, also I am an easygoing person and interested in Front End Development.
           </p>
           <a href="#" className="btn btn-outline-primary">
